95f55dbd7d5d94c4f86f8d91ad44cd81042f3c3d
Eldridge Turambi <eturambi95@gmail.com>
2022-05-15 14:14:12 -0600

    coverage commit

--- db/migrate/20220515194809_create_messages.rb
+++ db/migrate/20220515194809_create_messages.rb
@@ -2,10 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.string :body
-      t.references :chatrooms, foreign_key: true
-      t.references :users, foreign_key: true
-
-      t.timestamps
+      t.references :chatroom, foreign_key: true
+      t.references :user, foreign_key: true
     end
   end
 end
