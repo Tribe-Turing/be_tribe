0537bdd60a3ec349ec56df924440d51917730135
Eldridge Turambi <eturambi95@gmail.com>
2022-05-16 14:44:28 -0600

    Add: Serializer and controller for users

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,17 @@
+class UserSerializer
+
+  def self.all_users(users)
+    users.map do |user|
+      {
+        "id": user.id,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "pronouns": user.pronouns,
+        "image": user.picture,
+        "city": user.city,
+        "interests": user.interests.map { |interest|  interest.interest_name},
+        "bio": user.bio
+      }
+    end
+  end
+end
