b373f4ec1a419ca1ffef6edcebff457d2df4ef49
Eldridge Turambi <eturambi95@gmail.com>
2022-05-16 10:16:54 -0600

    Update: interest spec

--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Interest do
   describe 'validations' do
-    it { should validate_presence_of(:sports)}
-    it { should validate_presence_of(:night_life)}
-    it { should validate_presence_of(:networking)}
-    it { should validate_presence_of(:traveling)}
-    it { should validate_presence_of(:video_games)}
-    it { should validate_presence_of(:cinema)}
-    it { should validate_presence_of(:music)}
-    it { should validate_presence_of(:nature)}
-    it { should validate_presence_of(:food)}
-    it { should validate_presence_of(:art)}
-    it { should validate_presence_of(:tech)}
+    it { should validate_presence_of(:interest_name)}
   end
 
   describe 'relationships' do
