37df657cfc3bbd2cc0a810d6a845120078e3a085
Eldridge Turambi <eturambi95@gmail.com>
2022-05-16 11:21:37 -0600

    Refactored: Migrations for many to many between users and interests

--- db/migrate/20220516163053_create_interests.rb
+++ db/migrate/20220516163053_create_interests.rb
@@ -2,7 +2,6 @@ class CreateInterests < ActiveRecord::Migration[5.2]
   def change
     create_table :interests do |t|
       t.string :interest_name
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
