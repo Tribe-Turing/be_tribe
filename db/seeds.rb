bd1a5b17565f2c28cec474a40eb983447dbbe3dc
Eldridge Turambi <eturambi95@gmail.com>
2022-05-16 09:35:25 -0600

    Update: Seeds and Coverage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create!(first_name: 'firstname1', last_name: 'lastname1', pronouns: 'she/her', password_digest: 'password', bio: 'bio1', picture: 'profile pic link', )
+user1 = User.create!(first_name: 'Eldridge', last_name: 'Turambi', pronouns: 'he/him', password_digest: 'password', bio: 'self proclaimed cool kid from the block', picture: 'profile pic link', city: 'aurora')
+
 # user1.interests.create!(name: 'technology')
 # user1.interests.create!(name: 'clubbing')
 # user1.interests.create!(name: 'pets')
