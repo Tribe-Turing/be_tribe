e063273284603c3cfc9ecf225ee6a944f4441ba5
Eldridge Turambi <eturambi95@gmail.com>
2022-05-16 10:17:06 -0600

    Refactor: Interest model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,15 +1,5 @@
 class Interest < ApplicationRecord
-  validates_presence_of :sports
-  validates_presence_of :night_life
-  validates_presence_of :networking
-  validates_presence_of :traveling
-  validates_presence_of :video_games
-  validates_presence_of :cinema
-  validates_presence_of :music
-  validates_presence_of :nature
-  validates_presence_of :food
-  validates_presence_of :art
-  validates_presence_of :tech
-
   belongs_to :user
+
+  validates_presence_of :interest_name
 end
