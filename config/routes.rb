507c6e982c732c2caa717375b46caaaa78d991b5
Eldridge Turambi <eturambi95@gmail.com>
2022-05-16 14:44:07 -0600

    Add: Routing for all users api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      # get "/users", to: 'users#index'
+      resources :users, only: [:index]
+    end
+  end
   mount ActionCable.server => "/cable"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
