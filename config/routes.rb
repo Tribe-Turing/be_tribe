dd2ab1975b445328fa384aa476a90e2b9b4fb1d2
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-05-16 19:33:59 -0600

    Feat: merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
+      resources :messages
+      resources :conversations
     end
   end
+
   mount ActionCable.server => "/cable"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
