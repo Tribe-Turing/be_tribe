e1b8113d4c52b3c28193e4f17a629f0992ecd0ed
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-05-18 08:55:00 -0600

    Feat: Another cors attempt

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,11 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins '*'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    origins 'https://fe-tribe.herokuapp.com'
-    resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
