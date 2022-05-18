2f81f1fb678b33f86545a4be31ba4e183b3be7fc
Glsorensen <81265307+glsorensen@users.noreply.github.com>
2022-05-18 08:59:09 -0600

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,8 +17,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*',
-    headers: :any,
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
