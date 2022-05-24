c0db49b0c7e516866a28f14e92e4a52500ce3eb1
Sam Iavri <sammi80134@gmail.com>
2022-05-23 20:41:41 -0600

    ignore

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,37 +11,35 @@ class User < ApplicationRecord
   has_many :interests, through: :user_interests
   has_many :messages
 
-  has_many :convos_a, class_name: "Conversation", foreign_key: "user_a_id"
+  has_many :convos_a, class_name: 'Conversation', foreign_key: 'user_a_id'
   has_many :user_bs, through: :convos_a, source: :user_b
 
-  has_many :convos_b, class_name: "Conversation", foreign_key: "user_b_id"
+  has_many :convos_b, class_name: 'Conversation', foreign_key: 'user_b_id'
   has_many :user_as, through: :convos_b, source: :user_a
 
-
   def all_conversations
-        all_convos = []
+    all_convos = []
 
-        self.convos_a.each do |convo|
-
-            data = {
-                convo: convo,
-                user_a: User.find(convo.user_a_id),
-                user_b: User.find(convo.user_b_id),
-                messages: convo.messages
-            }
-            all_convos << data
-        end
-
-        self.convos_b.each do |convo|
-            data = {
-                convo: convo,
-                user_a: User.find(convo.user_a_id),
-                user_b: User.find(convo.user_b_id),
-                messages: convo.messages
-            }
-            all_convos << data
-        end
-
-        all_convos
+    convos_a.each do |convo|
+      data = {
+        convo: convo,
+        user_a: User.find(convo.user_a_id),
+        user_b: User.find(convo.user_b_id),
+        messages: convo.messages
+      }
+      all_convos << data
     end
+
+    convos_b.each do |convo|
+      data = {
+        convo: convo,
+        user_a: User.find(convo.user_a_id),
+        user_b: User.find(convo.user_b_id),
+        messages: convo.messages
+      }
+      all_convos << data
+    end
+
+    all_convos
   end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User do
-
   describe 'validations' do
-    it { should validate_presence_of(:first_name)}
-    it { should validate_presence_of(:last_name)}
-    it { should validate_presence_of(:pronouns)}
-    it { should validate_presence_of(:password_digest)}
-    it { should validate_presence_of(:bio)}
-    it { should validate_presence_of(:picture)}
-    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:pronouns) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:bio) }
+    it { should validate_presence_of(:picture) }
+    it { should validate_presence_of(:city) }
   end
 
   describe 'relationships' do
-    it { should have_many(:user_interests)}
-    it { should have_many(:interests).through(:user_interests)}
-    it { should have_many(:messages)}
+    it { should have_many(:user_interests) }
+    it { should have_many(:interests).through(:user_interests) }
+    it { should have_many(:messages) }
     it { should have_many(:convos_a) }
     it { should have_many(:user_bs) }
     it { should have_many(:convos_b) }
     it { should have_many(:user_as) }
   end
+
   it 'all_conversations is an array' do
     @eldridge = User.create!(first_name: 'Eldridge', last_name: 'Turambi', pronouns: 'he/him',
                              password_digest: 'password', bio: 'self proclaimed cool kid from the block', picture: 'profile pic link', city: 'denver,co')
