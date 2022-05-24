c2d9a02b41ffcbc1c95627fde7b6c49969b6dbcc
Dillon Parker <prkr93@gmail.com>
2022-05-24 12:15:26 -0600

    Fix for removing interests before creating interests from patch
    Co-Authored-By: Lauralyn Watson <lswatson16@users.noreply.github.com>
    Co-Authored-By: Gunnar <gunnar.l.sorensen@gmail.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,17 +12,14 @@ class Api::V1::UsersController < ApplicationController
 
   def update
     user = User.find_by(id: params[:id].to_i)
-    if user.update(user_params)
-      params[:interests].each do |interest|
-        if user.interests.include?(interest) == false
-          db_interest = Interest.find_by(interest_name: interest)
-          UserInterest.create!(user_id: user.id, interest_id: db_interest.id)
-        end
-      end
-      render json: UserSerializer.one_user(user)
-    else
-      render json: { status: 400, message: "Unable to update user" }
+    user.reset_interests
+
+    params[:interests].each do |interest|
+      db_interest = Interest.find_by(interest_name: interest)
+      new_interest = UserInterest.create!(user_id: user.id, interest_id: db_interest.id)
+      user.interests << db_interest
     end
+    render json: UserSerializer.one_user(user)
   end
 
   private
